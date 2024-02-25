52434251dec784c42b2d65383e354127fb47cf1f
Kliea <John.Farmer1086@gmail.com>
2024-02-25 02:22:58 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,472 @@
+﻿namespace EasyWorkApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            eCSNToolStripMenuItem = new ToolStripMenuItem();
+            CV_CHECK = new ToolStripMenuItem();
+            ESS_REPORTING = new ToolStripMenuItem();
+            QLIK_REPORTING = new ToolStripMenuItem();
+            CM_SHAREPOINT = new ToolStripMenuItem();
+            CM_BOOK = new ToolStripMenuItem();
+            PPS_SHAREPOINT = new ToolStripMenuItem();
+            PPS_BOOK = new ToolStripMenuItem();
+            MWWAC_DATASETS = new ToolStripMenuItem();
+            button_1 = new Button();
+            GroupBox_1 = new GroupBox();
+            button_2 = new Button();
+            button_3 = new Button();
+            button_4 = new Button();
+            groupBox2 = new GroupBox();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            GroupBox_2 = new GroupBox();
+            button_8 = new Button();
+            button_7 = new Button();
+            button_6 = new Button();
+            button_5 = new Button();
+            GroupBox_3 = new GroupBox();
+            groupBox5 = new GroupBox();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button_12 = new Button();
+            button_11 = new Button();
+            button_10 = new Button();
+            button_9 = new Button();
+            menuStrip1.SuspendLayout();
+            GroupBox_1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            GroupBox_2.SuspendLayout();
+            GroupBox_3.SuspendLayout();
+            groupBox5.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { eCSNToolStripMenuItem, CV_CHECK, ESS_REPORTING, QLIK_REPORTING, CM_SHAREPOINT, CM_BOOK, PPS_SHAREPOINT, PPS_BOOK, MWWAC_DATASETS });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Padding = new Padding(7, 2, 0, 2);
+            menuStrip1.Size = new Size(933, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // eCSNToolStripMenuItem
+            // 
+            eCSNToolStripMenuItem.Name = "eCSNToolStripMenuItem";
+            eCSNToolStripMenuItem.Size = new Size(48, 20);
+            eCSNToolStripMenuItem.Text = "eCSN";
+            eCSNToolStripMenuItem.Click += eCSNToolStripMenuItem_Click;
+            // 
+            // CV_CHECK
+            // 
+            CV_CHECK.Name = "CV_CHECK";
+            CV_CHECK.Size = new Size(75, 20);
+            CV_CHECK.Text = "CV CHECK";
+            CV_CHECK.Click += CV_CHECK_Click;
+            // 
+            // ESS_REPORTING
+            // 
+            ESS_REPORTING.Name = "ESS_REPORTING";
+            ESS_REPORTING.Size = new Size(101, 20);
+            ESS_REPORTING.Text = "ESS REPORTING";
+            ESS_REPORTING.Click += ESS_REPORTING_Click;
+            // 
+            // QLIK_REPORTING
+            // 
+            QLIK_REPORTING.Name = "QLIK_REPORTING";
+            QLIK_REPORTING.Size = new Size(108, 20);
+            QLIK_REPORTING.Text = "QLIK REPORTING";
+            QLIK_REPORTING.Click += QLIK_REPORTING_Click;
+            // 
+            // CM_SHAREPOINT
+            // 
+            CM_SHAREPOINT.Name = "CM_SHAREPOINT";
+            CM_SHAREPOINT.Size = new Size(111, 20);
+            CM_SHAREPOINT.Text = "CM SHAREPOINT";
+            CM_SHAREPOINT.Click += CM_SHAREPOINT_Click;
+            // 
+            // CM_BOOK
+            // 
+            CM_BOOK.Name = "CM_BOOK";
+            CM_BOOK.Size = new Size(73, 20);
+            CM_BOOK.Text = "CM BOOK";
+            CM_BOOK.Click += CM_BOOK_Click;
+            // 
+            // PPS_SHAREPOINT
+            // 
+            PPS_SHAREPOINT.Name = "PPS_SHAREPOINT";
+            PPS_SHAREPOINT.Size = new Size(112, 20);
+            PPS_SHAREPOINT.Text = "PPS SHAREPOINT";
+            PPS_SHAREPOINT.Click += PPS_SHAREPOINT_Click;
+            // 
+            // PPS_BOOK
+            // 
+            PPS_BOOK.Name = "PPS_BOOK";
+            PPS_BOOK.Size = new Size(74, 20);
+            PPS_BOOK.Text = "PPS BOOK";
+            PPS_BOOK.Click += PPS_BOOK_Click;
+            // 
+            // MWWAC_DATASETS
+            // 
+            MWWAC_DATASETS.Name = "MWWAC_DATASETS";
+            MWWAC_DATASETS.Size = new Size(123, 20);
+            MWWAC_DATASETS.Text = "MWWAC DATASETS";
+            MWWAC_DATASETS.Click += MWWAC_DATASETS_Click;
+            // 
+            // button_1
+            // 
+            button_1.Location = new Point(6, 28);
+            button_1.Name = "button_1";
+            button_1.Size = new Size(199, 170);
+            button_1.TabIndex = 1;
+            button_1.Text = "button1";
+            button_1.UseVisualStyleBackColor = true;
+            button_1.Click += button_1_Click;
+            // 
+            // GroupBox_1
+            // 
+            GroupBox_1.Controls.Add(groupBox2);
+            GroupBox_1.Controls.Add(button_4);
+            GroupBox_1.Controls.Add(button_3);
+            GroupBox_1.Controls.Add(button_2);
+            GroupBox_1.Controls.Add(button_1);
+            GroupBox_1.Location = new Point(36, 38);
+            GroupBox_1.Name = "GroupBox_1";
+            GroupBox_1.Size = new Size(866, 198);
+            GroupBox_1.TabIndex = 13;
+            GroupBox_1.TabStop = false;
+            GroupBox_1.Text = "GroupBox1";
+            GroupBox_1.Enter += groupBox1_Enter;
+            // 
+            // button_2
+            // 
+            button_2.Location = new Point(226, 28);
+            button_2.Name = "button_2";
+            button_2.Size = new Size(199, 170);
+            button_2.TabIndex = 2;
+            button_2.Text = "button2";
+            button_2.UseVisualStyleBackColor = true;
+            button_2.Click += button_2_Click;
+            // 
+            // button_3
+            // 
+            button_3.Location = new Point(440, 28);
+            button_3.Name = "button_3";
+            button_3.Size = new Size(199, 170);
+            button_3.TabIndex = 3;
+            button_3.Text = "button3";
+            button_3.UseVisualStyleBackColor = true;
+            button_3.Click += button_3_Click;
+            // 
+            // button_4
+            // 
+            button_4.Location = new Point(661, 28);
+            button_4.Name = "button_4";
+            button_4.Size = new Size(199, 170);
+            button_4.TabIndex = 4;
+            button_4.Text = "button4";
+            button_4.UseVisualStyleBackColor = true;
+            button_4.Click += button_4_Click;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button5);
+            groupBox2.Controls.Add(button6);
+            groupBox2.Controls.Add(button7);
+            groupBox2.Controls.Add(button8);
+            groupBox2.Location = new Point(6, 204);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(866, 198);
+            groupBox2.TabIndex = 14;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            // 
+            // button5
+            // 
+            button5.Location = new Point(661, 28);
+            button5.Name = "button5";
+            button5.Size = new Size(199, 170);
+            button5.TabIndex = 4;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(440, 28);
+            button6.Name = "button6";
+            button6.Size = new Size(199, 170);
+            button6.TabIndex = 3;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(226, 28);
+            button7.Name = "button7";
+            button7.Size = new Size(199, 170);
+            button7.TabIndex = 2;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(6, 28);
+            button8.Name = "button8";
+            button8.Size = new Size(199, 170);
+            button8.TabIndex = 1;
+            button8.Text = "button8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // GroupBox_2
+            // 
+            GroupBox_2.Controls.Add(button_8);
+            GroupBox_2.Controls.Add(button_7);
+            GroupBox_2.Controls.Add(button_6);
+            GroupBox_2.Controls.Add(button_5);
+            GroupBox_2.Location = new Point(36, 264);
+            GroupBox_2.Name = "GroupBox_2";
+            GroupBox_2.Size = new Size(866, 198);
+            GroupBox_2.TabIndex = 14;
+            GroupBox_2.TabStop = false;
+            GroupBox_2.Text = "GroupBox2";
+            GroupBox_2.Enter += GroupBox_2_Enter;
+            // 
+            // button_8
+            // 
+            button_8.Location = new Point(661, 28);
+            button_8.Name = "button_8";
+            button_8.Size = new Size(199, 170);
+            button_8.TabIndex = 4;
+            button_8.Text = "button8";
+            button_8.UseVisualStyleBackColor = true;
+            button_8.Click += button_8_Click;
+            // 
+            // button_7
+            // 
+            button_7.Location = new Point(440, 28);
+            button_7.Name = "button_7";
+            button_7.Size = new Size(199, 170);
+            button_7.TabIndex = 3;
+            button_7.Text = "button7";
+            button_7.UseVisualStyleBackColor = true;
+            button_7.Click += button_7_Click;
+            // 
+            // button_6
+            // 
+            button_6.Location = new Point(226, 28);
+            button_6.Name = "button_6";
+            button_6.Size = new Size(199, 170);
+            button_6.TabIndex = 2;
+            button_6.Text = "button6";
+            button_6.UseVisualStyleBackColor = true;
+            button_6.Click += button_6_Click;
+            // 
+            // button_5
+            // 
+            button_5.Location = new Point(6, 28);
+            button_5.Name = "button_5";
+            button_5.Size = new Size(199, 170);
+            button_5.TabIndex = 1;
+            button_5.Text = "button5";
+            button_5.UseVisualStyleBackColor = true;
+            button_5.Click += button_5_Click;
+            // 
+            // GroupBox_3
+            // 
+            GroupBox_3.Controls.Add(groupBox5);
+            GroupBox_3.Controls.Add(button_12);
+            GroupBox_3.Controls.Add(button_11);
+            GroupBox_3.Controls.Add(button_10);
+            GroupBox_3.Controls.Add(button_9);
+            GroupBox_3.Location = new Point(36, 497);
+            GroupBox_3.Name = "GroupBox_3";
+            GroupBox_3.Size = new Size(866, 198);
+            GroupBox_3.TabIndex = 15;
+            GroupBox_3.TabStop = false;
+            GroupBox_3.Text = "GroupBox3";
+            GroupBox_3.Enter += GroupBox_3_Enter;
+            // 
+            // groupBox5
+            // 
+            groupBox5.Controls.Add(button13);
+            groupBox5.Controls.Add(button14);
+            groupBox5.Controls.Add(button15);
+            groupBox5.Controls.Add(button16);
+            groupBox5.Location = new Point(6, 204);
+            groupBox5.Name = "groupBox5";
+            groupBox5.Size = new Size(866, 198);
+            groupBox5.TabIndex = 14;
+            groupBox5.TabStop = false;
+            groupBox5.Text = "groupBox5";
+            // 
+            // button13
+            // 
+            button13.Location = new Point(661, 28);
+            button13.Name = "button13";
+            button13.Size = new Size(199, 170);
+            button13.TabIndex = 4;
+            button13.Text = "button13";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(440, 28);
+            button14.Name = "button14";
+            button14.Size = new Size(199, 170);
+            button14.TabIndex = 3;
+            button14.Text = "button14";
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            button15.Location = new Point(226, 28);
+            button15.Name = "button15";
+            button15.Size = new Size(199, 170);
+            button15.TabIndex = 2;
+            button15.Text = "button15";
+            button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            button16.Location = new Point(6, 28);
+            button16.Name = "button16";
+            button16.Size = new Size(199, 170);
+            button16.TabIndex = 1;
+            button16.Text = "button16";
+            button16.UseVisualStyleBackColor = true;
+            // 
+            // button_12
+            // 
+            button_12.Location = new Point(661, 28);
+            button_12.Name = "button_12";
+            button_12.Size = new Size(199, 170);
+            button_12.TabIndex = 4;
+            button_12.Text = "button12";
+            button_12.UseVisualStyleBackColor = true;
+            button_12.Click += button_12_Click;
+            // 
+            // button_11
+            // 
+            button_11.Location = new Point(440, 28);
+            button_11.Name = "button_11";
+            button_11.Size = new Size(199, 170);
+            button_11.TabIndex = 3;
+            button_11.Text = "button11";
+            button_11.UseVisualStyleBackColor = true;
+            button_11.Click += button_11_Click;
+            // 
+            // button_10
+            // 
+            button_10.Location = new Point(226, 28);
+            button_10.Name = "button_10";
+            button_10.Size = new Size(199, 170);
+            button_10.TabIndex = 2;
+            button_10.Text = "button10";
+            button_10.UseVisualStyleBackColor = true;
+            button_10.Click += button_10_Click;
+            // 
+            // button_9
+            // 
+            button_9.Location = new Point(6, 28);
+            button_9.Name = "button_9";
+            button_9.Size = new Size(199, 170);
+            button_9.TabIndex = 1;
+            button_9.Text = "button9";
+            button_9.UseVisualStyleBackColor = true;
+            button_9.Click += button_9_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(933, 707);
+            Controls.Add(GroupBox_3);
+            Controls.Add(GroupBox_2);
+            Controls.Add(GroupBox_1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            GroupBox_1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            GroupBox_2.ResumeLayout(false);
+            GroupBox_3.ResumeLayout(false);
+            groupBox5.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem eCSNToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem CV_CHECK;
+        private System.Windows.Forms.ToolStripMenuItem ESS_REPORTING;
+        private System.Windows.Forms.ToolStripMenuItem QLIK_REPORTING;
+        private System.Windows.Forms.ToolStripMenuItem CM_SHAREPOINT;
+        private System.Windows.Forms.ToolStripMenuItem CM_BOOK;
+        private System.Windows.Forms.ToolStripMenuItem PPS_SHAREPOINT;
+        private System.Windows.Forms.ToolStripMenuItem PPS_BOOK;
+        private System.Windows.Forms.ToolStripMenuItem MWWAC_DATASETS;
+        private Button button_1;
+        private GroupBox GroupBox_1;
+        private GroupBox groupBox2;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button_4;
+        private Button button_3;
+        private Button button_2;
+        private GroupBox GroupBox_2;
+        private Button button_8;
+        private Button button_7;
+        private Button button_6;
+        private Button button_5;
+        private GroupBox GroupBox_3;
+        private GroupBox groupBox5;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button_12;
+        private Button button_11;
+        private Button button_10;
+        private Button button_9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,176 @@
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace EasyWorkApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Handle clicks on menu items to open corresponding websites
+        private void eCSNToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenWebsite("https://ecsn.gov.au/ESS/");
+        }
+
+        private void CV_CHECK_Click(object sender, EventArgs e)
+        {
+            OpenWebsite("https://marraworraworra.sharepoint.com/sites/MWWACCDP/SitePages/LearningTeamHome.aspx");
+        }
+
+        private void ESS_REPORTING_Click(object sender, EventArgs e)
+        {
+            OpenWebsite("https://reports.employment.gov.au/PORTAL/Reports");
+        }
+
+        private void QLIK_REPORTING_Click(object sender, EventArgs e)
+        {
+            OpenWebsite("https://pmc.dynamic.reports.employment.gov.au/bi/hub/my/favorites");
+        }
+
+        private void CM_SHAREPOINT_Click(object sender, EventArgs e)
+        {
+            OpenWebsite("https://marraworraworra.sharepoint.com/sites/CaseManagement/SitePages/Employee-onboarding-team-home.aspx");
+        }
+
+        private void CM_BOOK_Click(object sender, EventArgs e)
+        {
+            OpenWebsite("https://marraworraworra.sharepoint.com/:x:/r/sites/MWWACCDP/_layouts/15/Doc.aspx?sourcedoc=%7B8BCFEDC0-976F-4A6A-B3D5-594EE4471BCC%7D&file=CaseManagmentBook.xlsx&action=default&mobileredirect=true");
+        }
+
+        private void PPS_SHAREPOINT_Click(object sender, EventArgs e)
+        {
+            OpenWebsite("https://marraworraworra.sharepoint.com/sites/EmploymentPPS/SitePages/ProjectHome.aspx");
+        }
+
+        private void PPS_BOOK_Click(object sender, EventArgs e)
+        {
+            OpenWebsite("https://marraworraworra.sharepoint.com/:x:/r/sites/MWWACCDP/_layouts/15/Doc.aspx?sourcedoc=%7BB9347306-62A6-419D-B552-24182984EA3A%7D&file=PPSBook.xlsx&action=default&mobileredirect=true");
+        }
+
+        private void MWWAC_DATASETS_Click(object sender, EventArgs e)
+        {
+            OpenWebsite("https://marraworraworra.sharepoint.com/sites/MWWACCDP/SitePages/LearningTeamHome.aspx");
+        }
+
+        // Method to open a website in the default web browser
+        private void OpenWebsite(string url)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error opening website: " + ex.Message);
+            }
+        }
+
+        // Handle clicks on application buttons
+        private void button_1_Click(object sender, EventArgs e)
+        {
+            OpenApplication("mspaint");
+        }
+
+        private void button_2_Click(object sender, EventArgs e)
+        {
+            OpenApplication("winword");
+        }
+
+
+        private void button_3_Click(object sender, EventArgs e)
+        {
+            OpenApplication("excel");
+        }
+
+        private void button_4_Click(object sender, EventArgs e)
+        {
+            OpenApplication("powerpnt");
+        }
+
+        private void button_5_Click(object sender, EventArgs e)
+        {
+            OpenApplication("notepad");
+        }
+
+        private void button_6_Click(object sender, EventArgs e)
+        {
+            OpenApplication("notepad");
+        }
+
+        private void button_7_Click(object sender, EventArgs e)
+        {
+            OpenApplication("notepad");
+        }
+
+        private void button_8_Click(object sender, EventArgs e)
+        {
+            OpenApplication("notepad");
+        }
+
+        private void button_9_Click(object sender, EventArgs e)
+        {
+            OpenApplication("outlook.exe");
+        }
+
+        private void button_10_Click(object sender, EventArgs e)
+        {
+            OpenApplication("notepad");
+        }
+
+        private void button_11_Click(object sender, EventArgs e)
+        {
+            OpenApplication("Code");
+        }
+
+
+
+        private void button_12_Click(object sender, EventArgs e)
+        {
+            OpenApplication("powershell.exe");
+        }
+
+        // Method to open an application
+        private void OpenApplication(string command)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = command,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error opening application: " + ex.Message);
+            }
+        }
+
+        // Placeholder event handler methods
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+            // Placeholder for future functionality
+        }
+
+        private void GroupBox_2_Enter(object sender, EventArgs e)
+        {
+            // Placeholder for future functionality
+        }
+
+        private void GroupBox_3_Enter(object sender, EventArgs e)
+        {
+            // Placeholder for future functionality
+        }
+    }
+}
+
+
